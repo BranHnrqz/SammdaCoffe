537e53011197d6f14918b3d400537052a2c0fac7
Cristian Misael Lemus <87676318+CrisLemus21@users.noreply.github.com>
2023-06-05 12:30:35 -0600

    version(lemus)(produc)

---
+++ SammdaCoffe/SammdaCoffe/js/Products.js
@@ -0,0 +1,39 @@
+﻿@Html.ActionLink("Back to List", "Index")
+
+<style>
+    .form-container {
+        margin - top: 20px;
+    padding: 20px;
+    border: 1px solid #ccc;
+    border-radius: 5px;
+    }
+
+    .control-label {
+        font - weight: bold;
+    margin-bottom: 5px;
+    }
+
+    .form-control {
+        width: 100%;
+    padding: 8px;
+    border: 1px solid #ccc;
+    border-radius: 4px;
+    }
+
+    .text-danger {
+        color: red;
+    }
+
+    .btn {
+        padding: 8px 16px;
+    background-color: #4CAF50;
+    color: white;
+    border: none;
+    border-radius: 4px;
+    cursor: pointer;
+    }
+
+    .btn:hover {
+        background - color: #45a049;
+    }
+</style>
