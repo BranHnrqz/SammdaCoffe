599e76bc4bc85b664ec96f5cfbdc1b95d1e046ea
MigueGQ <87999546+MigueGQ@users.noreply.github.com>
2023-06-06 23:26:41 -0600

    Login

---
+++ SammdaCoffe/SammdaCoffe/Controllers/LoginController.cs
@@ -0,0 +1,40 @@
+﻿using SammdaCoffe.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SammdaCoffe.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        public ActionResult login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult login(string user, string paswo)
+        {
+
+            using (SammdasCoffeeEntities bd = new SammdasCoffeeEntities())
+            {
+                var list = bd.UserType.FirstOrDefault(a => a.userMail == user && a.userPassword == paswo);
+                if (list != null)
+                {
+
+                    return RedirectToAction("Menu", "Home");
+
+
+                }
+                else
+                {
+                    return View();
+                }
+            }
+
+        }
+    }
+}
