f639ab571905d28c0ad7e359735305a334408a3f
MigueGQ <87999546+MigueGQ@users.noreply.github.com>
2023-06-07 00:38:15 -0600

    v

--- SammdaCoffe/SammdaCoffe/Controllers/HomeController.cs
+++ SammdaCoffe/SammdaCoffe/Controllers/HomeController.cs
@@ -10,9 +10,16 @@ namespace SammdaCoffe.Controllers
     {
         public ActionResult Index()
         {
+
             return View();
         }
 
+
+        public ActionResult ListDProducts()
+        {
+            Response.Redirect("~/ProductDetails/ListdProducts");
+            return View();
+        }
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
